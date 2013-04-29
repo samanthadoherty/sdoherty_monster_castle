983eb1ecae189f22820115d89de6a7a6881407f5
Samantha Doherty <sdoherty@usc.edu>
2013-04-28 18:43:48 -0700

    Added main menu instantiation

--- game.cpp
+++ game.cpp
@@ -2,17 +2,21 @@
 #include <QtGui/QApplication>
 #include <QWidget>
 #include "mainwindow.h"
-#include "graphicswindow.h"
 #include "board.h"
+#include "mainmenu.h"
 
 using namespace std;
 
 int main (int argc, char *argv[]) {
  
    QApplication a(argc, argv);
-   MainWindow mw;
+   /*MainWindow mw;
    mw.addPushButtons();
    mw.addBottomButtons();
+   mw.addMoveButtons();
+   mw.addTextBoxes();*/
+   MainMenu mm;
+   mm.contButton();
    
    return a.exec();
 }
