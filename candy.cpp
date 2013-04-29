d8fcfed7a819ce79d6484bf37bb91e9bb2ba5ab2
Samantha Doherty <sdoherty@usc.edu>
2013-04-28 23:20:41 -0700

    Implemented candy class.

---
+++ candy.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "candy.h"
+
+using namespace std;
+
+Candy::Candy() {}
+
+Candy::Candy(int xLoc, int yLoc, Board* board)
+  : GamePiece(xLoc, yLoc, board) {
+  isMovingRight = true;
+}
+
+string Candy::display() {
+  return "images/candy1.png";
+}
+
+bool Candy::isCandy() {
+  return true;
+}
+
+bool Candy::isSpace() {
+  return false;
+}
+
+
+bool Candy::canMoveDown(vector<vector<GamePiece*> > *board) {
+  GamePiece* pieceBelow = board->at(x)[y];
+  return x + 1 != dim && !pieceBelow->isMonster();
+}
+
+bool Candy::canMoveDiagonalRight(vector<vector<GamePiece*> > *board) {
+ if (y + 1 == dim - 1) {
+    isMovingRight = false;
+  }
+  return y + 1 != dim - 1 && isMovingRight;
+}
+
+bool Candy::canMoveDiagonalLeft(vector<vector<GamePiece*> > *board) {
+  if (y == 1) {
+    isMovingRight = true;
+  }
+  return y != 1 && !isMovingRight;
+}
--- candy.h
+++ candy.h
@@ -1,7 +1,24 @@
 #ifndef CANDY_H
 #define CANDY_H
+#include <iostream>
+#include <string>
+#include "gamepiece.h"
+#include "board.h"
 
-class Candy : Gamepiece {
+using namespace std;
 
+class Candy : public GamePiece {
+
+public:
+  Candy();
+  Candy(int xLoc, int yLoc, Board* board);
+  string display();
+  bool isCandy();
+  bool isSpace();
+  bool canMoveDown(vector<vector<GamePiece*> > *board);  
+  bool canMoveDiagonalRight(vector<vector<GamePiece*> > *board);
+  bool canMoveDiagonalLeft(vector<vector<GamePiece*> > *board);
+private:
+  bool isMovingRight;
 };
 #endif
